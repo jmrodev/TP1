0a89e0103449324f1124c97f84c586361d995f65
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2022-05-12 17:03:18 -0300

    Casi terminado

--- lembikers/js/script.js
+++ lembikers/js/script.js
@@ -1,33 +1,34 @@
 "use strict";
-btn_captcha.addEventListener("click", ingresoCaptcha);
+let form  = document.querySelector("#btn_captcha");
+form.addEventListener("click", ingresoCaptcha);
 
 function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 let aleatorio = random(100, 999);
 
 let mensaje_captcha = document.querySelector("#mensaje_captcha");
-mensaje_captcha.innerHTML = "ingrese el numero siguiente " + aleatorio;
+mensaje_captcha.innerHTML = "Ingrese el siguiente " + aleatorio +" numero para verificar";
 
-function ingresoCaptcha() {
-   
-    let entrada = document.querySelector("#form_input");
-    let data_captcha = entrada.value;
-    let numero_captcha = parseInt(data_captcha);
-    let resultado_captcha = numero_captcha === aleatorio;
+function ingresoCaptcha(e) {
+  e.preventDefault();
+  let entrada = document.querySelector("#form_input");
+  let data_captcha = entrada.value;
+  let numero_captcha = parseInt(data_captcha);
+  let resultado_captcha = numero_captcha === aleatorio;
 
-    if (resultado_captcha) {
-        captchaCorrecto();
-    } else {
-        captchaIncorrecto();
-    }
+  if (resultado_captcha) {
+    captchaCorrecto();
+  } else {
+    captchaIncorrecto();
+  }
 }
 
 function captchaCorrecto() {
-    let entrada = document.querySelector("#mensaje_captcha")
-    entrada.innerHTML ="Usted ingreso el captcha correctamente";
+  let entrada = document.querySelector("#mensaje_captcha");
+  entrada.innerHTML = "Usted ingreso el captcha correctamente";
 }
 function captchaIncorrecto() {
-    let entrada = document.querySelector("#mensaje_captcha")
-    entrada.innerHTML = "Debes intentar nuevamente";
+  let entrada = document.querySelector("#mensaje_captcha");
+  entrada.innerHTML = "Debes intentar nuevamente";
 }
